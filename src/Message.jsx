c37a477fc812ee29f0a6d02ac23f3be6d9c1acdf
Amita <amita.parikh@gmail.com>
2019-04-08 23:22:24 +0000

    Added in first day of Chatty App work, new JSX files

---
+++ src/Message.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+    console.log('this.props:' , this.props);
+    return (
+        <div className="message">
+          <span className="message-username">{this.props.username}</span>
+          <span className="message-content">{this.props.messageContent}</span>
+        </div>
+    );
+  }
+}
+export default Message;
---
+++ src/MessageList.jsx
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  constructor(props){
+    super(props);
+    this.state = { messages: [
+      {
+        id: 1,
+        type: "incomingMessage",
+        content: "I won't be impressed with technology until I can download food.",
+        username: "Anonymous1"
+      },
+      {
+        id: 2,
+        type: "incomingNotification",
+        content: "Anonymous1 changed their name to nomnom",
+      },
+      {
+        id: 3,
+        type: "incomingMessage",
+        content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+        username: "Anonymous2"
+      },
+      {
+        id: 4,
+        type: "incomingMessage",
+        content: "...",
+        username: "nomnom"
+      },
+      {
+        id: 5,
+        type: "incomingMessage",
+        content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+        username: "Anonymous2"
+      },
+      {
+        id: 6,
+        type: "incomingMessage",
+        content: "This isn't funny. You're not funny",
+        username: "nomnom"
+      },
+      {
+        id: 7,
+        type: "incomingNotification",
+        content: "Anonymous2 changed their name to NotFunny",
+      },
+    ]
+  }
+  }
+
+  render() {
+    return (
+      <main className="messages">
+        {
+          this.state.messages.map((message) => {
+            return (
+              <Message key={message.id} username={message.username} messageContent={message.content} />
+            )
+          })
+        }
+      </main>
+    );
+  }
+}
+export default MessageList;

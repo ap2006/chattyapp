e4b61f0aaf86474ae005c94b778d64359085d7a3
Amita <amita.parikh@gmail.com>
2019-04-09 16:57:50 +0000

    Added in initial state and props to chatbar and messagelist

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,10 +3,11 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+// pass currentUser props to Chatbar
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser.name}/>
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
--- src/Message.jsx
+++ src/Message.jsx
@@ -2,11 +2,10 @@ import React, {Component} from 'react';
 
 class Message extends Component {
   render() {
-    console.log('this.props:' , this.props);
     return (
         <div className="message">
           <span className="message-username">{this.props.username}</span>
-          <span className="message-content">{this.props.messageContent}</span>
+          <span className="message-content">{this.props.content}</span>
         </div>
     );
   }

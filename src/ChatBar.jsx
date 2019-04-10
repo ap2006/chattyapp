6259665f1fa6703462e5de2cd9124d9776661e77
Amita <amita.parikh@gmail.com>
2019-04-10 20:05:51 +0000

    Added a server file

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,14 +3,21 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
-// pass currentUser props to Chatbar
+  handleEnter = (e) => {
+    if (e.key == "Enter") {
+      this.props.newMessage(e.target.value);
+      // this.socket.send(JSON.stringify(this.state.messages[this.state.messages.length-1]));
+    }
+  };
   render() {
+    console.log(this.props);
     return (
       <footer className="chatbar">
         <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser.name}/>
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.handleEnter} />
       </footer>
     );
   }
 }
+
 export default ChatBar;
